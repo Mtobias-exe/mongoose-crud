3bba6eaf0a816c82fbca8e8417f9be718eabc445
Mtobias-exe <maryfrance.tobias@gmail.com>
2024-12-19 14:07:17 -0500

    added seed data to the database

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const conn = require('./config/db')
+conn()
+const starterFruits = require('./config/seed')
+const Fruit = require('./models/fruit')
 
+//home route
+app.get('/', (req, res)=>{
+    res.send('Home Page')
+})
 
-
+//Seed route = populate
+app.get('/fruits/seed', async (req, res)=>{
+    try{
+        await Fruit.deleteMany({})
+        await Fruit.create(starterFruits)
+        res.json(starterFruits)
+    } catch (error) {
+        console.log(`Something went wrong loading seed data: ${error.message}`)
+    }
+})
 
 app.listen(port, () =>{
     console.log(`listening on port: ${port}`)
